04c581bb03a7bd629a7bc7bef836be0d48bf496f
Xardusen <120485316@qq.com>
2018-05-02 21:41:10 +0800

    5.2
    test on args(batch_size, filter kernel_size)

--- MyModels/detectionandtrain_short_2.py
+++ MyModels/detectionandtrain_short_2.py
@@ -23,29 +23,29 @@ def video_to_images(filename, n): # function to convert video to images
 
 
 def cnn_model_fn(features, labels, mode):  # model_fn for classifier
-    input_layer = tf.reshape(features["x"], [-1, 8, 8, 1])
-    conv1 = tf.layers.conv2d(
+    input_layer = tf.reshape(features["x"], [-1, 16, 16, 1])
+    conv1 = tf.layers.conv2d(  # input : [batch_size, 8, 8, 1] / output : [batch_size, 8, 8, 32]
         inputs=input_layer,
         filters=32,
-        kernel_size=[2, 2],
+        kernel_size=[3, 3],
         padding="same",
         activation=tf.nn.relu
     )
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-    conv2 = tf.layers.conv2d(
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)  # input : [batch_size, 16, 16, 32] / output : [batch_size, 8, 8, 32]
+    conv2 = tf.layers.conv2d(  # input : [batch_size, 8, 8, 32] / output : [batch_size, 8, 8, 64]
         inputs=pool1,
         filters=64,
-        kernel_size=[2, 2],
+        kernel_size=[3, 3],
         padding="same",
         activation=tf.nn.relu
     )
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-    pool2_flat = tf.reshape(pool2, [-1, 2* 2* 64])
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)  # input : [batch_size, 8, 8, 64] / output : [batch_size, 4, 4, 64]
+    pool2_flat = tf.reshape(pool2, [-1, 4* 4* 64])  # input : [batch_size, 4, 4, 64] / output : [batch_size, 4 * 4 * 64]
 
-    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)  # input : [batch_size, 4 * 4 * 64] / output : [batch_size, 1024]
     dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    logits = tf.layers.dense(inputs=dropout, units=15)
+    logits = tf.layers.dense(inputs=dropout, units=45)  # input : [batch_size, 1024] / output : [batch_size, 45]
     predictions = {
         "classes": tf.argmax(input=logits, axis=1),
         "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
@@ -53,6 +53,7 @@ def cnn_model_fn(features, labels, mode):  # model_fn for classifier
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
     loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
@@ -69,8 +70,8 @@ def cnn_model_fn(features, labels, mode):  # model_fn for classifier
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-Images_L = video_to_images("D:\QQBrowser\VideoData\\f5_dynamic_deint_L.avi", 110) # read video
-Images_R = video_to_images("D:\QQBrowser\VideoData\\f5_dynamic_deint_R.avi", 110)
+Images_L = video_to_images("D:\QQBrowser\VideoData\\f5_dynamic_deint_L.avi", 1100) # read video
+Images_R = video_to_images("D:\QQBrowser\VideoData\\f5_dynamic_deint_R.avi", 1100)
 
 kernel_size = [] # blur
 for image in Images_L:
@@ -97,40 +98,52 @@ for frame_r in Images_Rs:
 
 bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True) # use brute force matcher
 data, labels_all = [], []  # train and evaluate sources
-# query_ids of kp_L[0] in 100 frames (kp which shows over 20 times) :
-# {55: 44, 25: 43, 31: 43, 109: 41, 75: 33, 27: 32, 41: 31, 125: 30, 97: 29, 32: 28, 7: 24, 54: 24, 15: 23, 26: 22, 65: 20}
-# ->[55, 25, 31, 109, 75, 27, 41, 125, 97, 32, 7, 54, 15, 26, 65]
-chosen_ids = [7, 15, 25, 26, 27, 31, 32, 41, 54, 55, 65, 75, 97, 109, 125]
 
-for i in range(99):
+total_features = []  # calculate the chosen_ids
+for b in range(998):
+    matches = bf.match(des_L[0], des_L[b + 1])
+    matches = sorted(matches, key=lambda  x: x.distance)
+    for item in matches[: 10]:
+        total_features.append(item.queryIdx)
+res = Counter(total_features)
+query_xs = sorted(res, key=lambda x: res[x])
+chosen_ids = sorted(query_xs[-45:])  # query_ids of kp_L[0] in 1000 frames (kp which shows over 80 times)
+# print('total Counter result : ', res, '\n', 'result length : ', len(res), '\n', 'chosen_ids : ', chosen_ids)
+
+for i in range(998):
     matches = bf.match(des_L[0], des_L[i + 1])  # bf.match(querydes, traindes)
     matches = sorted(matches, key=lambda x: x.distance)
     data_temp, labels_temp = [], []
 
     for match in matches[: 10]:
         if match.queryIdx in chosen_ids:
-            roi = cv.getRectSubPix(Images_L[i + 1], (8, 8), kp_L[i + 1][match.trainIdx].pt)
+            roi = cv.getRectSubPix(Images_L[i + 1], (16, 16), kp_L[i + 1][match.trainIdx].pt)
             data_temp.append(cv.cvtColor(roi, cv.COLOR_BGR2GRAY))
             labels_temp.append(match.queryIdx)
 
     data.append(data_temp)
     labels_all.append(labels_temp)
 
-pattern = {7:0, 15:1, 25:2, 26:3, 27:4, 31:5, 32:6, 41:7, 54:8, 55:9, 65:10, 75:11, 97:12, 109:13, 125:14}  # convert labels to [1:15]
+
+translation = {}  # convert labels to [0:45]
+for i in range(45):
+    translation[chosen_ids[i]] = i
+
 labels2 = []
 for label in labels_all:
-    label2 = [pattern[x] if x in pattern else x for x in label]
+    label2 = [translation[x] if x in translation else x for x in label]
     labels2.append(label2)
 
 train_data, train_labels, eva_data, eva_labels = [], [], [], []  # training/evaluating data and label
-for roi_1 in data[:80]:
+for roi_1 in data[:800]:
     train_data.extend(roi_1)
-for num_1 in labels2[:80]:
+for num_1 in labels2[:800]:
     train_labels.extend(num_1)
-for roi_2 in data[80:]:
+for roi_2 in data[800:]:
     eva_data.extend(roi_2)
-for num_2 in labels2[80:]:
+for num_2 in labels2[800:]:
     eva_labels.extend(num_2)
+print(len(train_labels), len(train_data), len(eva_labels), len(eva_data))
 
 train_data = np.array(train_data, dtype=np.float32)
 eva_data = np.array(eva_data, dtype=np.float32)
@@ -140,24 +153,24 @@ eva_labels = np.array(eva_labels, dtype=np.int32)
 
 
 feature_classifier = tf.estimator.Estimator(
-    model_fn=cnn_model_fn, model_dir="/tmp/feature_net_model"
+    model_fn=cnn_model_fn, model_dir="/tmp/feature_net_model/1000_frames_bs_200"
 )  # create the classifier
 
 tensor_to_log = {"probabilities": "softmax_tensor"}  # to show information while processing
-logging_hook = tf.train.LoggingTensorHook(tensors=tensor_to_log, every_n_iter=50)
+logging_hook = tf.train.LoggingTensorHook(tensors=tensor_to_log, every_n_iter=500)
 
-# train_input_fn = tf.estimator.inputs.numpy_input_fn(
-#     x={"x": train_data},
-#     y=train_labels,
-#     batch_size=100,
-#     num_epochs=None,
-#     shuffle=True
-# )
-# feature_classifier.train(
-#     input_fn=train_input_fn,
-#     steps=5000,
-#     hooks=[logging_hook]
-# )
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": train_data},
+    y=train_labels,
+    batch_size=200,
+    num_epochs=None,
+    shuffle=True
+)
+feature_classifier.train(
+    input_fn=train_input_fn,
+    steps=3000,
+    # hooks=[logging_hook]
+)
 
 eval_input_fn = tf.estimator.inputs.numpy_input_fn(
     x={"x": eva_data},
@@ -168,78 +181,78 @@ eval_input_fn = tf.estimator.inputs.numpy_input_fn(
 eval_results = feature_classifier.evaluate(input_fn=eval_input_fn)
 print(eval_results)
 
-predict_data = []  # frame 101 processing
-for point in kp_L[100]:
-    img = cv.getRectSubPix(Images_L[100], (8, 8), point.pt)
-    predict_data.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
-predict_data = np.array(predict_data, dtype=np.float32)
-
-predict_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": predict_data},
-    y=None,
-    num_epochs=1,
-    shuffle=False
-)
-predict = feature_classifier.predict(input_fn=predict_input_fn)
-predict = list(item for item in predict)
-
-predict_seq, predict_prob, predict_labels = [], [], []
-for q in range(15):
-    predict_prob.append(0)
-    predict_labels.append(0)
-    predict_seq.append(0)
-
-for k in range(len(kp_L[100])):
-    predict[k]["probabilities"] = max(predict[k]["probabilities"])
-    for w in range(15):
-        if predict[k]["classes"] == w and predict[k]["probabilities"] > predict_prob[w]:
-            predict_seq[w] = k
-            predict_labels[w] = predict[k]["classes"]
-            predict_prob[w] = predict[k]["probabilities"]
-
-print(predict_seq,'\n', predict_labels, '\n', predict_prob)
-
-predict_data_1 = []  # frame 102 processing
-for point in kp_L[101]:
-    img = cv.getRectSubPix(Images_L[101], (8, 8), point.pt)
-    predict_data_1.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
-predict_data_1 = np.array(predict_data_1, dtype=np.float32)
-
-predict_input_fn_1 = tf.estimator.inputs.numpy_input_fn(
-    x={"x": predict_data_1},
-    y=None,
-    num_epochs=1,
-    shuffle=False
-)
-predict_1 = feature_classifier.predict(input_fn=predict_input_fn_1)
-predict_1 = list(item for item in predict_1)
-
-predict_seq_1, predict_prob_1, predict_labels_1 = [], [], []
-for q in range(15):
-    predict_prob_1.append(0)
-    predict_labels_1.append(0)
-    predict_seq_1.append(0)
-
-for k in range(len(kp_L[101])):
-    predict_1[k]["probabilities"] = max(predict_1[k]["probabilities"])
-    for w in range(15):
-        if predict_1[k]["classes"] == w and predict_1[k]["probabilities"] > predict_prob_1[w]:
-            predict_seq_1[w] = k  # used while matching
-            predict_labels_1[w] = predict_1[k]["classes"]
-            predict_prob_1[w] = predict_1[k]["probabilities"]
-
-print(predict_seq,'\n', predict_labels, '\n', predict_prob)
-print('\n', predict_seq_1,'\n', predict_labels_1, '\n', predict_prob_1)
-match_101_102 = bf.match(des_L[100], des_L[101], None)
-
-location = []
-for m in range(15):
-    match_101_102[m].queryIdx = predict_seq[m]
-    match_101_102[m].trainIdx = predict_seq_1[m]
-    location.append(math.sqrt(pow(kp_L[100][predict_seq[m]].pt[0]-kp_L[101][predict_seq_1[m]].pt[0], 2)+pow(kp_L[100][predict_seq[m]].pt[1]-kp_L[101][predict_seq_1[m]].pt[1], 2)))
-
-ma101_2 = cv.drawMatches(Images_L[100], kp_L[100], Images_L[101], kp_L[101], match_101_102[14:15], None, flags=2)
-
-cv.imshow('1', ma101_2)
-print(location)
-cv.waitKey(0)
+# predict_data = []  # frame 101 processing
+# for point in kp_L[100]:
+#     img = cv.getRectSubPix(Images_L[100], (8, 8), point.pt)
+#     predict_data.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
+# predict_data = np.array(predict_data, dtype=np.float32)
+#
+# predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     x={"x": predict_data},
+#     y=None,
+#     num_epochs=1,
+#     shuffle=False
+# )
+# predict = feature_classifier.predict(input_fn=predict_input_fn)
+# predict = list(item for item in predict)
+#
+# predict_seq, predict_prob, predict_labels = [], [], []
+# for q in range(15):
+#     predict_prob.append(0)
+#     predict_labels.append(0)
+#     predict_seq.append(0)
+#
+# for k in range(len(kp_L[100])):
+#     predict[k]["probabilities"] = max(predict[k]["probabilities"])
+#     for w in range(15):
+#         if predict[k]["classes"] == w and predict[k]["probabilities"] > predict_prob[w]:
+#             predict_seq[w] = k
+#             predict_labels[w] = predict[k]["classes"]
+#             predict_prob[w] = predict[k]["probabilities"]
+#
+# # print(predict_seq,'\n', predict_labels, '\n', predict_prob)
+#
+# predict_data_1 = []  # frame 102 processing
+# for point in kp_L[101]:
+#     img = cv.getRectSubPix(Images_L[101], (8, 8), point.pt)
+#     predict_data_1.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
+# predict_data_1 = np.array(predict_data_1, dtype=np.float32)
+#
+# predict_input_fn_1 = tf.estimator.inputs.numpy_input_fn(
+#     x={"x": predict_data_1},
+#     y=None,
+#     num_epochs=1,
+#     shuffle=False
+# )
+# predict_1 = feature_classifier.predict(input_fn=predict_input_fn_1)
+# predict_1 = list(item for item in predict_1)
+#
+# predict_seq_1, predict_prob_1, predict_labels_1 = [], [], []
+# for q in range(15):
+#     predict_prob_1.append(0)
+#     predict_labels_1.append(0)
+#     predict_seq_1.append(0)
+#
+# for k in range(len(kp_L[101])):
+#     predict_1[k]["probabilities"] = max(predict_1[k]["probabilities"])
+#     for w in range(15):
+#         if predict_1[k]["classes"] == w and predict_1[k]["probabilities"] > predict_prob_1[w]:
+#             predict_seq_1[w] = k  # used while matching
+#             predict_labels_1[w] = predict_1[k]["classes"]
+#             predict_prob_1[w] = predict_1[k]["probabilities"]
+#
+# # print(predict_seq,'\n', predict_labels, '\n', predict_prob)
+# # print('\n', predict_seq_1,'\n', predict_labels_1, '\n', predict_prob_1)
+# match_101_102 = bf.match(des_L[100], des_L[101], None)
+#
+# location = []
+# for m in range(15):
+#     match_101_102[m].queryIdx = predict_seq[m]
+#     match_101_102[m].trainIdx = predict_seq_1[m]
+#     location.append(math.sqrt(pow(kp_L[100][predict_seq[m]].pt[0]-kp_L[101][predict_seq_1[m]].pt[0], 2)+pow(kp_L[100][predict_seq[m]].pt[1]-kp_L[101][predict_seq_1[m]].pt[1], 2)))
+#
+# ma101_2 = cv.drawMatches(Images_L[100], kp_L[100], Images_L[101], kp_L[101], match_101_102[12:13], None, flags=2)
+#
+# # cv.imshow('1', ma101_2)
+# print(location)
+# cv.waitKey(0)
--- MyModels/test_doc.py
+++ MyModels/test_doc.py
@@ -1,16 +1,15 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
-import math
+MORSE_CODE = {'1':'sad', '2': 're', '3': 'gg'}
 
-
-
-mnist = input_data.read_data_sets("MNIST_data/")
-train_data = mnist.train.images  # Returns np.array
-train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-eval_data = mnist.test.images  # Returns np.array
-eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-
-print(type(train_data), type(train_data[0]), type(train_data[0][0]))
-print(type(train_labels), type(train_labels[0]))
-print(train_labels[0])
+def decodeMorse(morse_code):
+    # ToDo: Accept dots, dashes and spaces, return human-readable message
+    morse_code = morse_code.strip()
+    out = ''
+    checking = ' '
+    while morse_code:
+        out += MORSE_CODE[morse_code[: morse_code.index(checking)]] + checking
+        morse_code = morse_code[morse_code.index(checking) + 1 :]
+        if checking not in morse_code:
+            out += MORSE_CODE[morse_code]
+            morse_code = ''
+    return out
+print(decodeMorse('1 2 3'))
